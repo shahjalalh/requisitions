a43d888c6f431059dfc3fc730336fbdbde6eb9ee
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-20 15:38:48 +0600

    odoo 12 requisitions module

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Requisition',
+    'version': '1.0',
+    'author': "Shahjalal Hossain",
+    'category': 'Purchase',
+    'summary': 'Requisition',
+    'description': "Requisition",
+    'website': '',
+    'depends': ['purchase'],
+    'data': [
+        'views/requisition_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ models/requisition.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.osv import expression
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_compare
+from odoo.exceptions import UserError, AccessError
+from odoo.tools.misc import formatLang
+from odoo.addons import decimal_precision as dp
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    requisition_number = fields.Char('PO Requisition')
+
+class Requisition(models.Model):
+    _name = "po.requisition"
+    
+    name = fields.Char('PO Requisition')
+    warehouse_ids = fields.Many2many('stock.warehouse', string='Warehouses', required=True,
+        help="Show the routes that apply on selected warehouses.")
+    delivery_date = fields.Datetime(string='Delivery Date', required=True, index=True)
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('approve', 'Approve'),
+        ('cancel', 'Cancelled')
+    ], string='Status', readonly=True, index=True, copy=False, default='draft', track_visibility='onchange')
+    order_line = fields.One2many('requisition.order.line', 'order_id', string='Order Lines', states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True)
+
+    @api.multi
+    def action_approve_po_requisition(self):
+
+        import pdb;pdb.set_trace()
+
+        return True
+
+
+class RequisitionOrderLine(models.Model):
+    _name = 'requisition.order.line'
+    _description = 'Requisition Order Line'
+
+
+    order_id = fields.Many2one('po.requisition', string='Order Reference', index=True, required=True, ondelete='cascade')
+    name = fields.Text(string='Description', required=True)
+    product_id = fields.Many2one('product.product', string='Product', domain=[('purchase_ok', '=', True)], change_default=True, required=True)
+    product_qty = fields.Float(string='Quantity', digits=dp.get_precision('Product Unit of Measure'), required=True)
+    
+    product_uom = fields.Many2one('uom.uom', string='Product Unit of Measure', required=True)
+
+    
