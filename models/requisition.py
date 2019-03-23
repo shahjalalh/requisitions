5124cdd9dd0f8597640a8d3c604c60e1edcfd404
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-23 14:47:25 +0600

    create po from requisition

--- models/requisition.py
+++ models/requisition.py
@@ -15,32 +15,64 @@ class PurchaseOrder(models.Model):
 
     requisition_number = fields.Char('PO Requisition')
 
+
 class Requisition(models.Model):
     _name = "po.requisition"
-    
-    name = fields.Char('PO Requisition')
-    warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse', required=True)
-    delivery_date = fields.Datetime(string='Delivery Date', required=True, index=True)
 
+    name = fields.Char('PO Requisition')
+    warehouse_id = fields.Many2one(
+        'stock.warehouse', string='Warehouse', required=True)
+    delivery_date = fields.Datetime(
+        string='Delivery Date', required=True, index=True)
+    po_reference = fields.Many2one('purchase.order', string='PO Reference')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('approve', 'Approve'),
         ('cancel', 'Cancelled')
     ], string='Status', readonly=True, index=True, copy=False, default='draft', track_visibility='onchange')
-    order_line = fields.One2many('requisition.order.line', 'order_id', string='Order Lines', states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True)
+    partner_id = fields.Many2one('res.partner', string='Vendor',
+                                 help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
+    order_line = fields.One2many('requisition.order.line', 'order_id', string='Order Lines', states={
+                                 'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True)
 
     @api.multi
     def action_approve_po_requisition(self):
 
-        import pdb;pdb.set_trace()
+        po_data = {
+            'requisition_number': str(self.name),
+            'date_order': fields.datetime.now(),
+            'partner_id': self.partner_id,
+
+        }
+
+        po_line_list = list()
+
+        for line_item in self.order_line:
+            po_line_list.append([0, False,
+                                 {
+                                     'product_id': line_item.product_id,
+                                     'product_qty': line_item.product_qty,
+                                     'product_uom': line_item.product_uom,
+
+                                 }])
+
+        po_data['order_line'] = po_line_list
+
+        # create PO
+        qc_env = self.pool.get('purchase.order')
+        saved_po_id = qc_env.create(
+            self._cr, self._uid, po_data, context=self._context)
+
+        # Update PO Reference and state
+        req_update_query = "UPDATE po_requisition SET po_reference='{0}', state='approve' WHERE id={1}".format(
+            saved_po_id, self.id)
 
         return True
 
-    
     @api.model
     def create(self, values):
         record = super(Requisition, self).create(values)
-        
+
         record.name = "REQ0"+str(record.id)
 
         return record
@@ -50,12 +82,13 @@ class RequisitionOrderLine(models.Model):
     _name = 'requisition.order.line'
     _description = 'Requisition Order Line'
 
-
-    order_id = fields.Many2one('po.requisition', string='Order Reference', index=True, required=True, ondelete='cascade')
+    order_id = fields.Many2one(
+        'po.requisition', string='Order Reference', index=True, required=True, ondelete='cascade')
     name = fields.Text(string='Description')
-    product_id = fields.Many2one('product.product', string='Product', domain=[('purchase_ok', '=', True)], change_default=True, required=True)
-    product_qty = fields.Float(string='Quantity', digits=dp.get_precision('Product Unit of Measure'), required=True)
-    
-    product_uom = fields.Many2one('uom.uom', string='Product Unit of Measure', required=True)
+    product_id = fields.Many2one('product.product', string='Product', domain=[
+                                 ('purchase_ok', '=', True)], change_default=True, required=True)
+    product_qty = fields.Float(string='Quantity', digits=dp.get_precision(
+        'Product Unit of Measure'), required=True)
 
-    
+    product_uom = fields.Many2one(
+        'uom.uom', string='Product Unit of Measure', required=True)
