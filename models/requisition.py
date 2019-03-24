9ccdb50a91c1e2e979bbc32f5f1727437fc65f97
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2019-03-24 15:02:19 +0600

    clean up

--- models/requisition.py
+++ models/requisition.py
@@ -60,14 +60,10 @@ class Requisition(models.Model):
 
         po_data['order_line'] = po_line_list
 
-        # import pdb;pdb.set_trace()
         # create PO
         po_env = self.env['purchase.order'].create(po_data)
         saved_po_id = po_env.create(po_data)
         
-        # po_env = self.pool.get('purchase.order')
-        # saved_po_id = po_env.create(self._cr, self._uid, po_data, context=self._context)
-
         # Update PO Reference and state
         req_update_query = "UPDATE po_requisition SET po_reference={0}, state='approve' WHERE id={1}".format(
             saved_po_id.id, self.id)
